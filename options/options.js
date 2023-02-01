9e557a2f99eaa1eea3c8a865b0347fffc9282f50
Crowdbotics <engineering@crowdbotics.com>
2023-02-01 00:26:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-poetry-dev-60420",
+  url: "https://long-poetry-dev-60420.botics.co",
+  api: "https://long-poetry-dev-60420.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
